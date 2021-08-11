6cfd2b1cd39464493158538d2e97b1ba23ebaa11
gramm2009 <viktorkaturkin@gmail.com>
2021-08-11 11:49:15 +0300

    first commit

---
+++ src/layout/AdminPanelLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminPanelLayout:React.FC =()=> {
+    
+    
+    return (
+        <div className="app">
+            
+        </div>
+    )
+}
+
+export default  AdminPanelLayout
