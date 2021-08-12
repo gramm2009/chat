30130fdd45fe3ff7f161a387ab4b89324c1a5429
gramm2009 <viktorkaturkin@gmail.com>
2021-08-12 14:40:50 +0300

    end chats header

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,16 @@
+import { FlashOff } from '@material-ui/icons';
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Chat () {
+
+    const [ searchValue, setSearchValue ] = useState<string>( '' )
+
+    const fff = ( e: React.MouseEvent ) => {
+        const element = e.target as HTMLElement
+    }
+
     return (
         <div className="chat">
 
@@ -18,17 +27,21 @@ export default function Chat () {
                         + Create New Chat
                     </button>
 
-                    <div className="chat__chat-list__header__search">
-                        <input type="text" />
-                        <div className="chat__chat-list__header__search__menu">Message</div>
-                        <div>Search chat</div>
+                    <div className="chat__chat-list__header__search" onClick={ ( e ) => fff( e ) }>
+                        <input
+                            type="text"
+                            onChange={ ( e ) => setSearchValue( e.target.value ) }
+                            className="chat__chat-list__header__search__input"
+                        />
+                        <div className="chat__chat-list__header__search__menu">Chat</div>
+                        <div className="chat__chat-list__header__search__icon"></div>
                     </div>
                 </div>
 
                 <div className="chat__chat-list__body"></div>
             </div>
 
-            {/* <div className="chat__chat-sms">САМ КОНКРЕТНЫЙ ЧАМ</div> */}
+            {/* <div className="chat__chat-sms">САМ КОНКРЕТНЫЙ ЧАМ</div> */ }
         </div>
     );
 }
